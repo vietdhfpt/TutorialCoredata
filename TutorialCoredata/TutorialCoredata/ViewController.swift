ebd64c628ecfc3f674002ef065403b82c246fe8d
Do Hoang Viet <vietdh2@fsoft.com.vn>
2018-08-24 22:07:14 +0700

    Init project

---
+++ TutorialCoredata/TutorialCoredata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TutorialCoredata
+//
+//  Created by Do Hoang Viet on 8/24/18.
+//  Copyright © 2018 Do Hoang Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
